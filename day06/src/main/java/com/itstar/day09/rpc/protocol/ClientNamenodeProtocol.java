a16665198feeed56aa26677fa2914768eb888eda
叶华来了 <hengyufxh1@163.com>
2019-02-13 16:49:08 +0800

    delete
    delete

--- day06/src/main/java/com/itstar/day09/rpc/protocol/ClientNamenodeProtocol.java
+++
@@ -1,8 +0,0 @@
-package com.itstar.day09.rpc.protocol;
-
-public interface ClientNamenodeProtocol {
-
-    public static final long versionID =1L;
-    // 拿到元数据信息 hdfs dfs -cat /8888
-    public String getMetaData(String path);
-}
